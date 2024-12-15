b4b737e2a353621a0414642f5b7bbe0239ffd218
Jakub Duda <121130603+Dudu7352@users.noreply.github.com>
2024-12-15 20:41:32 +0100

    descriptions for --help

--- src/args.rs
+++ src/args.rs
@@ -1,18 +1,21 @@
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(version, about, long_about = None)]
+#[command(version, about = "Utility that displays GIFs in terminal", long_about = None)]
 pub struct DisplayArgs {
+    /// GIF file to display
     pub file: String,
 
+    /// Width to display. Without this flag GIF will be stretched to fill the screen
     #[arg(short = 'W', long)]
     pub width: Option<u32>,
 
-    #[arg(long = "loop", action)]
+    /// Loops animation infinitely, press Ctrl-C to escape
+    #[arg(long = "loop")]
     pub loop_animation: bool,
 }
 
 pub enum DisplaySize {
     Width(u32),
-    Fill
+    Fill,
 }
